c34e404eff50d7db6b3f9f66e37ef42848a11328
ogamertv12 <39074420+ogamertv12@users.noreply.github.com>
2020-01-08 17:59:41 +0700

    Update Test1.lua

--- Test1.lua
+++ Test1.lua
@@ -191,7 +191,6 @@ while(true) do
 			  "checkbox",
 			  "checkbox",
 			  "checkbox",
-			  "checkbox",
         })
         if i ~= nil then
             if i[1] then gg.setVisible(false) gg.toast("หวัดดี") gg.setVisible(true) end
@@ -201,7 +200,7 @@ while(true) do
 			if i[5] then gg.setVisible(false) Cheat(Hack["MEO4"]) gg.setVisible(false) end
 			if i[6] then gg.setVisible(false) Cheat(Hack["MEO5"]) gg.setVisible(false) end
 			if i[7] then gg.setVisible(false) Cheat(Hack["MEO6"]) gg.setVisible(false) end
-			if i[8] then gg.setVisible(false) Cheat(Hack["MEO6"]) gg.setVisible(false) end
+			if i[8] then gg.setVisible(false) Cheat(Hack["MEO7"]) gg.setVisible(false) end
 			if i[9] then gg.setVisible(false) ChangeFOV(Hack["FOV"]) gg.setVisible(false) end 
 			if i[10] then gg.setVisible(false) break gg.setVisible(false) end
 		end
