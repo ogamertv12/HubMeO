ca55d5514a6715fb5e0e21ac0a4b56224c4dfaea
ogamertv12 <39074420+ogamertv12@users.noreply.github.com>
2020-01-08 18:07:02 +0700

    Update Test1.lua

--- Test1.lua
+++ Test1.lua
@@ -192,7 +192,6 @@ while(true) do
 			  "checkbox",
 			  "checkbox",
 			  "checkbox",
-			  "checkbox",
         })
         if i ~= nil then
             if i[1] then gg.setVisible(false) gg.toast("หวัดดี") gg.setVisible(true) end
